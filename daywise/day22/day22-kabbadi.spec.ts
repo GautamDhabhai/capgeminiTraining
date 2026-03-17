fff48c0ecfad2bbb7d8b4adc289c87173e81aacd
GautamDhabhai <gautamdhabhai@gmail.com>
2026-03-17 21:45:47 +0530

    day22

---
+++ daywise/day22/day22-kabbadi.spec.ts
@@ -0,0 +1,15 @@
+import {test} from '../../playwrightt/node_modules/@playwright/test'
+
+test("kabbadi", async({page}) => {
+    await page.goto('https://www.prokabaddi.com/schedule-fixtures-results?tab=recent')
+
+    let elems = await page.locator('//div[@class="fixtures-listing"][1]/div[1]/h2 | //div[@class="fixtures-listing"][1]/div[2]/div[@class="fixtures-group"]/div[@class="fixtures-element"][1]/div[@class="element element1"]/p[@class="match-count"] | //div[@class="fixtures-listing"][1]/div[2]/div[@class="fixtures-group"]/div[@class="fixtures-element"]/div[@class="element element2"]/div[@class="element-wrap"]/a//div[@class="team-info"]//p[@class="team-name"] | //div[@class="fixtures-listing"][1]/div[1]/h2 | //div[@class="fixtures-listing"][1]/div[2]/div[@class="fixtures-group"]/div[@class="fixtures-element"][1]/div[@class="element element1"]/p[@class="match-count"] | //div[@class="fixtures-listing"][1]/div[2]/div[@class="fixtures-group"]/div[@class="fixtures-element"]/div[@class="element element2"]/div[@class="element-wrap"]/a//p[@class="score"] | //div[@class="fixtures-listing"][1]/div[1]/h2 | //div[@class="fixtures-listing"][1]/div[2]/div[@class="fixtures-group"]/div[@class="fixtures-element"][1]/div[@class="element element1"]/p[@class="match-count"] | //div[@class="fixtures-listing"][1]/div[2]/div[@class="fixtures-group"]/div[@class="fixtures-element"]/div[@class="element element3"]/p').allTextContents();
+
+    for(let text of elems){
+        console.log(text);
+    }
+})
+
+
+
+//div[@class="fixtures-listing"][1]/div[1]/h2 | //div[@class="fixtures-listing"][1]/div[2]/div[@class="fixtures-group"]/div[@class="fixtures-element"][1]/div[@class="element element1"]/p[@class="match-count"] | //div[@class="fixtures-listing"][1]/div[2]/div[@class="fixtures-group"]/div[@class="fixtures-element"]/div[@class="element element2"]/div[@class="element-wrap"]/a//div[@class="team-info"]//p[@class="team-name"] | //div[@class="fixtures-listing"][1]/div[1]/h2 | //div[@class="fixtures-listing"][1]/div[2]/div[@class="fixtures-group"]/div[@class="fixtures-element"][1]/div[@class="element element1"]/p[@class="match-count"] | //div[@class="fixtures-listing"][1]/div[2]/div[@class="fixtures-group"]/div[@class="fixtures-element"]/div[@class="element element2"]/div[@class="element-wrap"]/a//p[@class="score"] | //div[@class="fixtures-listing"][1]/div[1]/h2 | //div[@class="fixtures-listing"][1]/div[2]/div[@class="fixtures-group"]/div[@class="fixtures-element"][1]/div[@class="element element1"]/p[@class="match-count"] | //div[@class="fixtures-listing"][1]/div[2]/div[@class="fixtures-group"]/div[@class="fixtures-element"]/div[@class="element element3"]/p
